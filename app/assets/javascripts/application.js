2a58424ddaca43b17d878b7873964a91db73f048
Justin Taj <justintaj@yahoo.com>
2019-02-24 15:19:56 -0800

    stylcss layout implemented (rough draft)

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,12 @@
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
+//= require jquery3
+//= require popper
+//= require bootstrap
 //= require_tree .
+$( document ).ready(function() {
+
+
+
+});
